78e309d8d8ae64d9e695f4c26684aa83fb44c79e
nidarg <gradin_g@yahoo.com>
2022-09-04 23:04:56 +0300

    gg

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 
 import path from 'path';
-import {fileURLToPath} from 'url';
+// import {fileURLToPath} from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
+// const __filename = fileURLToPath(import.meta.url);
 
 
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 import config from './config.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -48,9 +48,9 @@ cloudinary.config({
   api_secret:config.CLOUD_API_SECRET,
 })
 // app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
-app.use(express.static(path.resolve(__dirname, '/frontend')));
+app.use(express.static(path.join(__dirname, '/../frontend')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '/frontend,index.html'));
+  res.sendFile(path.join(__dirname, '/../frontend/index.html'));
 });
 app.use((err, req, res, next) => {
   const status = err.name && err.name === 'ValidationError' ? 400 : 500;
